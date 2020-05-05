d2f439a82146e709de297614a42c4a3b2fe5a4ae
rathma <wrathofrathma@gmail.com>
2020-05-04 21:26:51 -0400

    Added bulma css framework & created index page.

---
+++ index.js
@@ -0,0 +1,120 @@
+var rand_btn = document.querySelector("#random_btn");
+var create_btn = document.querySelector("#create_btn");
+var create_text = document.querySelector("#create_text");
+var connected_text = document.querySelector("#connected");
+var form = document.querySelector("#submit");
+var room_container = document.querySelector("#rooms");
+//For some reason I had to set this because it wasn't clearing on refresh?
+create_text.value=null;
+
+function join_random(){
+    window.location.assign("https://webrtc.kyso.dev/rand")
+}
+
+function attempt_join(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onload = () => {
+        var resp = JSON.parse(xhttp.response);
+
+        if(resp.uuid){
+            console.log("Room found");
+            if(resp.avail>0){
+                //redirect to room
+                window.location.assign(resp.url);
+            }
+            else{
+                alert("Room is full");
+            }
+        }
+        else{
+            form.submit();
+        }
+    };
+
+    var r = create_text.value;
+    if(r === ""){
+        join_random();
+    }
+    else{
+        xhttp.open("GET", "/getroom/" + create_text.value, true);
+        xhttp.send();
+    }
+}
+
+rand_btn.onclick = () => {
+    join_random();
+};
+create_btn.onclick = () => {
+    attempt_join();
+
+};
+create_text.onkeydown = () => {
+    if(event.keyCode==13){
+        attempt_join();
+     }
+};
+
+// Updates the # connected users
+function update_connected(val){
+    connected.innerHTML = "Active Rooms - Connected " + val;
+}
+
+function join_room(id){
+    console.log(id.attributes.room_id);
+    window.location.assign("https://webrtc.kyso.dev/room/" + id.attributes.room_id.nodeValue);
+}
+
+//Formats server room objects for the list
+function format_room(val){
+    var btn_color = "is-success";
+    var btn_text = "Join";
+    var fn = "onclick=join_room(this)";
+    if(val.avail==0){
+        btn_color = "is-danger";
+        btn_text = "Full";
+        fn = "";
+    }
+    console.log(val.uuid);
+    var html = `
+<div class="columns">
+    <div class="column is-narrow">
+        <a class="button ` + btn_color +`" room_id="` + val.uuid + `" ` + fn + `>` + btn_text + ` `
+        + val.user_count + "/" + val.capacity +
+    `
+    </a>
+    </div>
+    <div class="column is-four-fifths">`
+        + val.name + `
+    </div>
+</div>
+`;
+    return html;
+
+}
+//Updates the room list
+function update_rooms(vals){
+    var rooms = [];
+    var thtml = "";
+    Object.keys(vals).forEach(k => {
+        console.log(vals[k]);
+        thtml += format_room(vals[k]);
+        rooms.push(format_room(vals[k]));
+    });
+    console.log(rooms);
+
+    room_container.innerHTML = thtml;
+}
+
+socket = new WebSocket("wss://webrtc.kyso.dev/index");
+
+socket.onmessage = async function (event) {
+    var data = JSON.parse(event.data);
+    switch(data.method){
+        case "room_update":
+            update_rooms(data.rooms);
+            break;
+        case "connected_update":
+            update_connected(data.connected);
+            break;
+    }
+}
--- server.py
+++ server.py
@@ -7,26 +7,54 @@ import json
 from aiortc import RTCPeerConnection, RTCSessionDescription, RTCConfiguration, RTCIceServer
 from copy import deepcopy
 from sanic import Sanic
-from sanic.response import file, text
+from sanic.response import file, text, redirect
+from sanic.response import json as r_json
 from sanic.websocket import WebSocketProtocol, ConnectionClosed
 import pathlib
 import ssl
 
-# This file is a functional demonstration of a signaling server connecting two clients directly.
-# When two users are connected, the signaling server will tell one of the clients to initiate the request.
-#
-# Communication in the signaling server will be handled using JSON & Serialization
-# Each request will contain two minimum properties
-# - method: that will tell the client what to do with the request.
-# - uuid: if incoming, then the intended destination uuid, if outgoing, the senders uuid
-# Defined methods
-# - id: The user's id
-# - init: Initialize an offer with the passed uuid
-# - offer: An SDP offer to be passed to the uuid
-# - answer: an SDP answer to be passed to the uuid
+# Peers connected to the main index
+index_peers = {}
 
+# Peers connected to the webrtc area
 peers = {}
 rooms = {}
+room_names = {}
+
+async def push_room_update():
+    """Pushes the updated room list info to all users on the index.
+    """
+    for user,ws in index_peers.items():
+        await send_rooms(ws)
+
+async def send_rooms(ws):
+    payload = { "method": "room_update", "rooms": rooms }
+    await ws.send(json.dumps(payload))
+
+
+
+def create_room(params, rid=None):
+    print(params)
+    if(rid is None):
+        room_id = str(uuid4())
+    else:
+        room_id = rid
+    rooms[room_id] = {}
+    print(params["room_name"])
+    print(params["room_name"][0])
+    url = "https://webrtc.kyso.dev/room/" + room_id
+    room_names[params["room_name"][0]] = room_id
+    rooms[room_id]["uuid"] = room_id
+    rooms[room_id]["name"] = params["room_name"]
+    rooms[room_id]["url"] = url
+    rooms[room_id]["capacity"] = params["participants"]
+    rooms[room_id]["users"] = []
+    rooms[room_id]["chat"] = []
+    rooms[room_id]["avail"] = params["participants"]
+    rooms[room_id]["user_count"] = 0
+    print("Created room %s" % str(rooms[room_id]))
+    asyncio.ensure_future(push_room_update())
+    return url
 
 async def on_disconnect(user):
     print("Connection closed - removing user")
@@ -62,8 +90,35 @@ def get_other_peer(uid):
         if(key!=uid):
             return key
 
-
 app = Sanic("WebRTC Example")
+async def on_index_disconnect(user):
+    index_peers.pop(user)
+    await update_connected_index()
+
+async def update_connected_index():
+    for k,ws in index_peers.items():
+        await ws.send(json.dumps({"method": "connected_update", "connected": len(index_peers)}))
+
+
+@app.websocket('/index')
+async def ws_index(request, websocket):
+    user = str(uuid4())
+    index_peers[user] = websocket
+
+    await update_connected_index()
+    await send_rooms(websocket)
+    while(websocket.open):
+        try:
+            recv = await websocket.recv();
+        except:
+            await on_index_disconnect(user)
+
+
+    
+
+@app.websocket('/rtc')
+async def ws_rtc(request, websocket):
+    pass
 
 @app.websocket('/sock')
 async def on_connect(request, websocket):
@@ -83,35 +138,64 @@ async def on_connect(request, websocket):
 
     await asyncio.ensure_future(peer_listener(user, websocket))
 
-def create_room(tag):
-    rooms[tag] = {"users": [], "chat": []}
 
-@app.route('/room/<tag>')
-async def serve_room(request, tag):
-    print("Got a request for room tag {:s}".format(tag))
-    if(tag not in rooms.keys()):
-        create_room(tag)
+@app.route('/getroom/<key>')
+async def get_room_data(request, key):
+    print("Requested room data for %s" % str(key))
+    if(key not in room_names.keys()):
+        return r_json({})
+    _id = room_names[key]
+    room_data = {
+        "name": rooms[_id]["name"],
+        "uuid": _id
+    }
 
-    # Max 2 members per room
-    if(len(rooms[tag]["users"])>=2):
-        return text("room at maximum occupancy")
+    return r_json(room_data)
+# This allows us to share a link of a room we created
+@app.route('/room/<key>')
+async def serve_room(request, key):
+    print("Got a request for room key {:s}".format(key))
+    # If it doesn't exist, send them home
+    if(key not in rooms.keys()):
+        print("Room not found.")
+        return await file("index.html")
+    return await file("room.html")
 
-    return await file("index.html")
+# This is the method called from the index to create a room
+@app.route('/room', methods=["POST"])
+async def serve_create_room(request):
+    print("Serving room")
+    print(request.form)
+    room_data = request.form
+    url = create_room(room_data)
+    return redirect(url)
 
+
+@app.route('/rand')
+async def serve_rand_room(request):
+    rid = str(uuid4())
+    url = create_room({"room_name": rid, "participants": ["2"]}, rid)
+    return redirect(url)
+
+@app.route('/favicon.ico')
+async def favicon(request):
+    return await file('favicon.ico')
 
 @app.route('/')
 async def index(request):
-    print("Index request!")
+    print("Index request from: %s" % str(request.ip))
     return await file("index.html")
 
 @app.route('/webrtc.js')
-async def javascript(request):
-    print("Javascript request")
+async def webrtc_js(request):
     return await file("webrtc.js")
+
+@app.route('/index.js')
+async def index_js(request):
+    return await file("index.js")
 
 @app.route("/style.css")
 async def css(request):
-    print("CSS request")
     return await file("style.css")
 
 if __name__=="__main__":
